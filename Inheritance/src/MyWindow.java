4c5fb9419f921ce040d56e6b81970b78b10c64ee
rodolfomsantos <rodolfo.m.santos@gmail.com>
2017-03-19 12:06:21 +0000

    Inheretance_Subclass

---
+++ Inheritance/src/MyWindow.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class MyWindow extends JFrame{
+
+	public static void main(String[] args) {
+		
+		new MyWindow();
+		
+	}
+	
+	public MyWindow (){
+		setSize(500, 500);
+		setVisible(true);
+		setTitle("My Window");
+	}
+	
+}
